aed36b58fdd1ea5c3db78a79c6b00a5b9ee3b5fa
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2023-03-11 21:14:32 +0900

    ルーティングとヘッダーレイアウトの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Home from './pages/home/Home';
-import { Login } from '@mui/icons-material';
+import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+
 import theme from './theme/theme';
+import { Router } from './router/Router';
 
 
 export default function MyApp() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <p>あああああああ</p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
