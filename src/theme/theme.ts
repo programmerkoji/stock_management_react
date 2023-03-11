8831ad01e672b4c37c3d161f9633d0ca239aee33
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2023-03-11 19:59:40 +0900

    react-router install

---
+++ src/theme/theme.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
+
+export default theme;
